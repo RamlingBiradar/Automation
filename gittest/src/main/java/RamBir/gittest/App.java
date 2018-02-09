a553fb9a8a5c1f9316382eb2a15068837067072c
Ramling <Ramling@Ramling>
2018-02-09 18:30:20 +0530

    Ram Git test demo..

---
+++ gittest/src/main/java/RamBir/gittest/App.java
@@ -0,0 +1,13 @@
+package RamBir.gittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
