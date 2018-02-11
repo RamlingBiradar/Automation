bf23682a6bd46d84b07014b541546f765ecf3b2c
RamlingBiradar <34168569+RamlingBiradar@users.noreply.github.com>
2018-02-11 13:05:26 +0530

    Add files via upload

---
+++ gittest/src/test/java/RamBir/gittest/NewTest.java
@@ -0,0 +1,9 @@
+package RamBir.gittest;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
---
+++ gittest/src/test/java/RamBir/gittest/testgit.java
@@ -0,0 +1,12 @@
+package RamBir.gittest;
+
+import org.testng.annotations.Test;
+
+public class testgit {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Github config success");
+	  System.out.println("Github config success1");
+  }
+}
